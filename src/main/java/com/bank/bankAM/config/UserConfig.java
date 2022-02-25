dfd4ee795770386b80431584a39a32bd2e64a4a7
Moussatef <otman.moussetaf@gmail.com>
2022-02-25 10:08:25 +0100

    Docker

--- src/main/java/com/bank/bankAM/config/UserConfig.java
+++ src/main/java/com/bank/bankAM/config/UserConfig.java
@@ -16,8 +16,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
-
-@Configuration @RequiredArgsConstructor
+ @RequiredArgsConstructor
 public class UserConfig {
 
     private final PasswordEncoder passwordEncoder;
